b6cfee7e5a841253fc537ca52929cb3e30400a79
NPC-MARIMO <shivangbhaiisgreat@gmail.com>
2024-10-16 16:33:58 +0530

    second

--- app/layout.jsx
+++ app/layout.jsx
@@ -10,7 +10,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
-        <Provider>
+        
         <div className="main">
           <div className="gradient"/>
         </div>
@@ -19,7 +19,6 @@ export default function RootLayout({ children }) {
           <Nav/>
           {children}
         </main>
-        </Provider>
       </body>
     </html>
   )
